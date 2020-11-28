b2b93883ba675087aff81a87f5123f85867ce538
Petar <japatrisko97@gmail.com>
2020-11-28 16:05:11 +0100

    Connected bank-microservice with Eureka

--- KP/banka-service/src/main/java/com/bankaservice/backend/BankaServiceApplication.java
+++ KP/banka-service/src/main/java/com/bankaservice/backend/BankaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.bankaservice.backend.bankaservice;
+package com.bankaservice.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
