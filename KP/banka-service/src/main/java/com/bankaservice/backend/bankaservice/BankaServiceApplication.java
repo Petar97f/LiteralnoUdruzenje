31ad4a6a37e307e2834ac08fbb807cb3711f62cc
Petar <japatrisko97@gmail.com>
2020-11-28 15:11:13 +0100

    Added initial version of bank service

---
+++ KP/banka-service/src/main/java/com/bankaservice/backend/bankaservice/BankaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bankaservice.backend.bankaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankaServiceApplication.class, args);
+	}
+
+}
---
+++ KP/banka-service/src/test/java/com/bankaservice/backend/bankaservice/BankaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankaservice.backend.bankaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
