c8791546b3dd2d6d06d005e26ed566f82a003b78
Petar <japatrisko97@gmail.com>
2020-12-22 19:40:31 +0100

    Created more entities

--- upp/src/main/resources/data.sql
+++ upp/src/main/resources/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO user (name,surname,email,password, activated, user_role)
 VALUES ('User1','Prezime1','user1@gmail.com','$2y$12$jMfmhs1lwpKGSOeLeXkVSu/GOR3vXwG8ICVacerwNFXPKogaf/cq6', true, 0);
+INSERT INTO user (name,surname,email,password, activated, user_role)
+VALUES ('User2','Prezime2','user2@gmail.com','$2y$12$jMfmhs1lwpKGSOeLeXkVSu/GOR3vXwG8ICVacerwNFXPKogaf/cq6', true, 0);
 
 INSERT INTO genre (name)
 VALUES ('horor');
