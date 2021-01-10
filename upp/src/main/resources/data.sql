2ca7159bf32a47f6ce58397515b18c7fac0d00f3
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2021-01-10 23:20:21 +0100

    update sql file

--- upp/src/main/resources/data.sql
+++ upp/src/main/resources/data.sql
@@ -10,4 +10,7 @@ INSERT INTO genre (name)
 VALUES ('comedy');
 
 INSERT INTO book (id,name,authorId,publisherId,genre,yearOfIssue,editors,lectors,numOfPages,price)
-VALUES (1L,'Mali princ','1','1','knjiga','2020-11-11','nema','nema',33,100.00);
+VALUES (1,'Mali princ','1','1','knjiga','2020-11-11','nema','nema',33,100.00);
+
+INSERT INTO book (id,name,authorId,publisherId,genre,yearOfIssue,editors,lectors,numOfPages,price)
+VALUES (2,'Veliki princ','1','1','kopija','2020-11-12','nema','nema',71,900.00);
