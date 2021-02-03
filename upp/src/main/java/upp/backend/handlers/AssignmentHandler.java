ce3df02c2786ad0413bbe997b3eeae9a47d53285
Ivana <sankovicivana27@gmail.com>
2021-02-03 22:16:09 +0100

    commit after merge problems

--- upp/src/main/java/upp/backend/handlers/AssignmentHandler.java
+++ upp/src/main/java/upp/backend/handlers/AssignmentHandler.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package upp.backend.handlers;
 
 import java.util.List;
@@ -37,21 +36,4 @@ public class AssignmentHandler implements TaskListener  {
 	}
 
 }
-=======
-package upp.backend.handlers;
 
-import org.camunda.bpm.engine.delegate.DelegateTask;
-import org.camunda.bpm.engine.delegate.TaskListener;
-
-public class AssignmentHandler implements TaskListener  {
-
-	@Override
-	public void notify(DelegateTask delegateTask) {
-		// TODO Auto-generated method stub
-	    //delegateTask.setAssignee("kermit");
-	    //delegateTask.addCandidateUser("fozzie");
-	    //delegateTask.addCandidateGroup("management");
-	}
-
-}
->>>>>>> main
--- upp/src/main/java/upp/backend/handlers/GetMembers.java
+++ upp/src/main/java/upp/backend/handlers/GetMembers.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package upp.backend.handlers;
 
 import java.util.ArrayList;
@@ -62,40 +61,3 @@ public class GetMembers  implements JavaDelegate {
 	}
 
 }
-=======
-package upp.backend.handlers;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.camunda.bpm.engine.IdentityService;
-import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.camunda.bpm.engine.identity.Group;
-import org.camunda.bpm.engine.identity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import upp.backend.service.UserService;
-
-@Service
-public class GetMembers  implements JavaDelegate {
-
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private IdentityService identityService;
-	 
-	@Override
-	public void execute(DelegateExecution execution) throws Exception {
-		// TODO Auto-generated method stub
-		List<User> users = identityService.createUserQuery().memberOfGroup("members").list();
-
-		
-		execution.setVariable("numberOfMembers", users.size());
-		
-	}
-
-}
->>>>>>> main
