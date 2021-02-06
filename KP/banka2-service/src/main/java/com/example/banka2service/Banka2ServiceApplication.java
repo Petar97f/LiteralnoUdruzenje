6705d395a7444266c5bb53620167a67ffa4e58f4
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2021-02-06 14:53:30 +0100

    update security

--- KP/banka2-service/src/main/java/com/example/banka2service/Banka2ServiceApplication.java
+++ KP/banka2-service/src/main/java/com/example/banka2service/Banka2ServiceApplication.java
@@ -1,15 +1,26 @@
 package com.example.banka2service;
 
+import com.example.banka2service.service.CardService;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import javax.annotation.PostConstruct;
+
 @EnableFeignClients
 @SpringBootApplication
 public class Banka2ServiceApplication {
-
+	@Autowired
+	CardService cardService;
 	public static void main(String[] args) {
 		SpringApplication.run(Banka2ServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	private void codes(){
+		cardService.code();
 	}
 
 }
